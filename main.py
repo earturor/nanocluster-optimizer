08e39a83f68b79c3aea3947615289560393cc126
earturor <earturordr@outlook.com>
2023-02-13 15:17:16 -0700

    Added a new package to generate a random cluster

--- main.py
+++ main.py
@@ -3,8 +3,20 @@
 # email: earturordr@outlook.com
 
 from potentials import gupta
+from packages import rnd_cluster
 
 def main():
+    # Generate a random cluster
+    file_path_out = './outputs/test-rnd.xyz'
+    atoms = {'Fe':2, 'Co':3, 'Ni':4}
+    rnd_cluster.random_cluster(file_path_out, atoms)
+
+    gupta.FILE_PATH_IN = file_path_out
+    gupta.FILE_PATH_OUT = './outputs/test-rnd-opt.xyz'
+    energy, x = gupta.optimize()
+    print("Energy", energy)
+
+'''
     # Type the path of the cluster
     gupta.FILE_PATH_IN = './inputs/test.xyz'
     gupta.FILE_PATH_OUT = './outputs/test-opt.xyz'
@@ -12,6 +24,7 @@ def main():
     # Optimize the cluster
     energy, x = gupta.optimize()
     print("Energy:", energy)
+'''
     
 
 if __name__ == '__main__':
---
+++ packages/rnd_cluster.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def random_cluster(file_path_out, atoms, lb=2.56, ub=-2.56):
+    atoms_list = []
+    for key in atoms:
+        for i in range(atoms[key]):
+            atoms_list.append(key)
+
+    n_atoms = len(atoms_list)
+    x0 = np.random.uniform(lb, ub, (n_atoms, 3))
+    x0 = x0.flatten()
+    write_new_file_xyz(x0, file_path_out, atoms_list)
+
+def write_new_file_xyz(x, file, atoms_list):
+    n_atoms = len(atoms_list)
+    with open(file, 'w') as file_object:
+        file_object.write(f'{int(n_atoms)}\n')
+        file_object.write('\n')
+        i = 0
+        j = 1
+        k = 2
+        for n in range(n_atoms):
+            file_object.write('{}   {: .8f}       {: .8f}       {: .8f}\n'.format(atoms_list[n], x[i], x[j], x[k]))
+            i += 3
+            j += 3
+            k += 3
