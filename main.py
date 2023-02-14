0cea9788f29ab1235d3e4551c268bf60e128e25d
earturor <earturordr@outlook.com>
2023-02-14 13:30:07 -0700

    Added a random cluster generator and optimize it

--- main.py
+++ main.py
@@ -2,27 +2,35 @@
 # Arturo Rentería
 # email: earturordr@outlook.com
 
+import pandas as pd
 from potentials import gupta
 from packages import rnd_cluster
 
 def main():
-    # Generate a random cluster
-    file_path_out = './outputs/test-rnd.xyz'
-    atoms = {'Fe':2, 'Co':3, 'Ni':4}
-    rnd_cluster.random_cluster(file_path_out, atoms)
-
-    gupta.FILE_PATH_IN = file_path_out
-    gupta.FILE_PATH_OUT = './outputs/test-rnd-opt.xyz'
-    energy, x = gupta.optimize()
-    print("Energy", energy)
+    # Generates a certain number of random clusters and subsequently optimizes them.
+    epochs = 25 # Type the number of cluster to be generated
+    energies = []
+    files = []
+    for i in range(epochs):
+        # Generate a random cluster
+        file_path_out = './outputs/test-rnd.xyz'
+        atoms = {'Fe':0, 'Co':0, 'Ni':7} # Type the elements and their quantities
+        rnd_cluster.random(file_path_out, atoms)
+        # Specifies the input and output path and then optimize the cluster
+        gupta.FILE_PATH_IN = file_path_out
+        gupta.FILE_PATH_OUT = f'./outputs/test-rnd-opt{i}.xyz'
+        energy, x = gupta.optimize()
+        print("Energy", energy)
+        # Create a energy table in csv
+        energies.append(float(energy))
+        files.append(str(f'test-rnd-opt{i}.xyz'))
+        df = pd.DataFrame({ "File": files, "Energy": energies })
+        df.to_csv('./outputs/energy_table.csv')
 
 '''
-    # Optimize a cluster from a specific file
-    # Type the path of the cluster
+    # Optimize a cluster from a specific path
     gupta.FILE_PATH_IN = './inputs/test.xyz'
-    gupta.FILE_PATH_OUT = './outputs/test-opt.xyz'
-    
-    # Optimize the cluster
+    gupta.FILE_PATH_OUT = './outputs/test-opt.xyz'    
     energy, x = gupta.optimize()
     print("Energy:", energy)
 '''
